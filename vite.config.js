1bc37b8d9e155359bb4b313f9e3a9bcb9bf9c48e
Olli Tiainen <olli.vj.tiainen@gmail.com>
2025-11-21 15:16:03 +0200

    fgasedrsgaersrg

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/advent-calendar-new/', // Replace with your repo name
 })
